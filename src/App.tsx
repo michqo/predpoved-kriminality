90ddf56ea1182c1bf85e58f2ad15eb0c97f5cd92
michqo <42279279+michqo@users.noreply.github.com>
2024-05-14 10:00:17 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from '@/components/ui/button';
+
+function App() {
+  return (
+    <>
+      <div className="flex gap-x-3 h-screen w-full items-center justify-center">
+        <Button>2030</Button>
+        <Button>2035</Button>
+      </div>
+    </>
+  );
+}
+
+export default App;
