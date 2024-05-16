77ee452a37642aa234fe94ba8a0fddc6e3f572f4
michqo <42279279+michqo@users.noreply.github.com>
2024-05-16 11:06:25 +0200

    feat: year input form

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,69 @@
-import { DialogDescription, DialogHeader, DialogTitle } from '@/components/ui/dialog';
-import Modal from './components/Modal';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+
+import { Button } from '@/components/ui/button';
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+
+const FormSchema = z.object({
+  year: z.coerce
+    .number({ required_error: 'Rok je požadovaný.', invalid_type_error: 'Rok musí byť číslo.' })
+    .positive({
+      message: 'Rok musi byt viac ako 0'
+    })
+    .min(2020, {
+      message: 'Rok musi byt aspon 2020.'
+    })
+});
 
 function App() {
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      // @ts-expect-error: Blank value
+      year: ''
+    }
+  });
+
+  function onSubmit(data: z.infer<typeof FormSchema>) {
+    console.log(`You submitted the following values: \n${JSON.stringify(data, null, 2)}`);
+  }
+
   return (
-    <div className="relative grid h-screen grid-flow-row grid-rows-3">
+    <div className="relative grid h-screen grid-flow-row grid-rows-3 justify-center">
       <div className="relative row-span-1 flex flex-col items-center bg-background text-foreground">
         <h1 className="mt-12 text-4xl font-extrabold text-slate-800">PREDPOVEĎ KRIMINALITY</h1>
         <h2 className="text-2xl font-bold text-blue-500">VYBRAŤ ROK</h2>
       </div>
-
-      <div className="row-span-2 grid w-full grid-flow-col items-stretch">
-        <Modal text="2030">
-          <DialogHeader>
-            <DialogTitle>Predpoveď pre rok 2030</DialogTitle>
-            <DialogDescription>Údaje kriminality</DialogDescription>
-          </DialogHeader>
-        </Modal>
-        <Modal text="2035">
-          <DialogHeader>
-            <DialogTitle>Predpoveď pre rok 2035</DialogTitle>
-            <DialogDescription>Údaje kriminality</DialogDescription>
-          </DialogHeader>
-        </Modal>
+      <div className="row-span-2 flex w-screen max-w-md flex-col">
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="w-full space-y-6">
+            <FormField
+              control={form.control}
+              name="year"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Rok</FormLabel>
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+                  <FormDescription>Rok na predpoveď.</FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit">Submit</Button>
+          </form>
+        </Form>
       </div>
       <img src="/icon.png" alt="My Image" className="absolute ml-5 h-56 w-72 object-contain" />
     </div>
